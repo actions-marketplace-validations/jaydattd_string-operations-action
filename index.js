12854744ce5084218ded56f3617a71410b30b9aa
jaydattd <jaydatt.desai@sophos.com>
2023-09-13 14:06:00 +0530

    Use vercel/ncc

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  
+    const stringToConvert = core.getInput('string_to_process');
+    console.log(`String to process ${stringToConvert}!`);
+    
+    const lowercase = stringToConvert.toLowerCase();
+    console.log(`toLowerCase: ${lowercase}`);
+    core.setOutput("toLowerCase", lowercase);
+
+    const uppercase = stringToConvert.toUpperCase();
+    console.log(`toUperCase: ${uppercase}`);
+    core.setOutput("toUperCase", uppercase);
+    
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
